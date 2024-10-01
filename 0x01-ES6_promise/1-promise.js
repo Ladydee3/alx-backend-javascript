87a696a70c7d7cc1219b83fcca2498151e2d3c0d
Ladydee3 <delisiletwala62@gmail.com>
2024-10-01 10:19:17 -0700

    1. Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
-export default function getFullResponseFromAPI(cuccess) {
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve({
-				status: 200,
-			body: 'success',
-			});
-		} else {
-			reject(Error('The Fake API is not working currently'));
-		}
-	});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
 
