0a4b57ae31a39f457b305a1ec39a623ff744a78d
Ladydee3 <delisiletwala62@gmail.com>
2024-10-03 07:37:22 -0700

    classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,5 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
-  });
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
